0c3a37d6657ee01a912aec6554de699d8107a743
jakk <timaeus@utexas.edu>
2016-03-21 18:00:20 -0700

    Added ActionKiller class to Actuator.java: spins off a thread that waits 100ms and sets actions to 0 if they have not been updated.

--- Actuator.java
+++ Actuator.java
@@ -7,10 +7,12 @@ class Actuator implements Runnable {
 
     Physics physics;
     private ObjectInputStream in;
+    protected boolean actionsUpdated;
 
     Actuator(Physics phy, ObjectInputStream in) {
         this.physics = phy;
         this.in = in;
+        actionsUpdated = false;
     }
 
     void init() {
@@ -24,15 +26,45 @@ class Actuator implements Runnable {
     public synchronized void run() {
         while (true) {
             try {
-              // read action data from control server  
+              // read action data from control server
               Object obj = in.readObject();
               double[] data = (double[]) (obj);
               assert(data.length == physics.NUM_POLES);
+              actionsUpdated = true;
               physics.update_actions(data);
+              ActionKiller actionKiller = new ActionKiller(physics);
+              Thread actionKillThread = new Thread(actionKiller);
+              actionKillThread.start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
+    }
+
+    private class ActionKiller implements Runnable {
+      Physics physics;
+
+      ActionKiller(Physics phy) {
+        actionsUpdated = false;
+        this.physics = phy;
+      }
+
+      public synchronized void run() {
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          if (!actionsUpdated) {
+            // simplest implementation: update actions to be 0
+            double[] data = new double[physics.NUM_POLES];
+            for (double d : data) {
+              d = 0;
+            }
+            physics.update_actions(data);
+          }
+      }
+
     }
 }
